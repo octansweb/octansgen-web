5ff330b0b467a809ea80e0c141a9b353e68a6b24
Muneeb Qadoos <iammmuneeb@gmail.com>
2024-05-31 18:49:39 +0100

    Update sign up button to be disabled

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,13 +200,15 @@
               </span>
             </label>
             <div class="hidden sm:flex">
-              <a href="signin.html" class="loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70">
+              <a href="https://octansgen.com/app/login" class="loginBtn px-[22px] py-2 text-base font-medium text-white hover:opacity-70">
                 Sign In
               </a>
-              <a href="signup.html"
-                class="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+              <button 
+                href="signup.html"
+                disabled
+                class="disabled:opacity-50 disabled:cursor-not-allowed signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
                 Sign Up
-              </a>
+              </button>
             </div>
           </div>
         </div>
